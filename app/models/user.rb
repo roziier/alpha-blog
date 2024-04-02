f966fac9e25ad544e7d04e3c995f4e1f1a4a2ec2
roziier <71633856+roziier@users.noreply.github.com>
2024-04-02 12:34:45 +0100

    adding secure password
    adding secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                uniqueness: {case_sensitive: false}, 
                length: {maximum: 60},
                format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_095322) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_112259) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_095322) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
