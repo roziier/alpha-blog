0c3aaf5778fda9a0237f678f08257a500a4b59c2
roziier <71633856+roziier@users.noreply.github.com>
2024-04-02 12:02:12 +0100

    added user and article association
    Added user and article association, added user shown in the browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
         if @article.save
           flash[:notice] = "Article was created successfully"
           redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_084043) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_095322) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
